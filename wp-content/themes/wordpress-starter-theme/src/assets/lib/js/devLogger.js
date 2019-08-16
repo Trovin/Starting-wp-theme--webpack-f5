f443301bdb16a0cd4165fb1b351e7a4b7526be04
wex <ruslan.ffive@gmail.com>
2019-08-16 18:43:09 +0300

    init

---
+++ wp-content/themes/wordpress-starter-theme/src/assets/lib/js/devLogger.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+class DevLogger {
+    constructor() {
+        this.colors = { notify: '#006400', warning: '#808000', error: 'red' };
+        this.style = `
+            padding: 3px 15px;
+            font-size: 14px;
+            color: #fff;
+            font-weight: bold;
+            border-radius: 10px;
+        `;
+    }
+
+    logInProduction() {
+        if(!isDev) console.log(...arguments);
+    }
+
+    notify() {
+        this.log("%cNotify", `${this.style}background: ${this.colors.notify};`, ...arguments);
+    }
+
+    warning() {
+        this.log("%cWarning", `${this.style}background-color: ${this.colors.warning};`, ...arguments);
+    }
+
+    error() {
+        this.log("%cError", `${this.style}background-color: ${this.colors.error};`, ...arguments);
+    }
+
+    handler(handler) {
+        if(isDev) handler();
+    }
+
+    log() {
+        if(isDev) console.log(...arguments);
+    }
+}
+module.exports = new DevLogger();
